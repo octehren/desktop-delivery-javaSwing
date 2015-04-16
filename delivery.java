21836e4709cbc9f67dab53c1bbb08c39b793bfda
otamm <oivatom@gmail.com>
2015-04-15 22:43:33 -0300

    Added logic & windows to button action

--- delivery.java
+++ delivery.java
@@ -6,7 +6,8 @@ public class Delivery extends JFrame
 {
 	public static void main(String[] args)
 	{
-		new Delivery();
+		Delivery delivery = new Delivery();
+		delivery.setLocationRelativeTo(null);
 	}
 
 	private JButton buttonOK;
@@ -79,6 +80,47 @@ public class Delivery extends JFrame
 
 	public void buttonOKClick()
 	{
-		
+		String products = "";
+		String happyThankYou = "\nHappy-Thank-You-Message: \n";
+
+		if (burger.isSelected()) 
+		{ 
+			products += "Burger \n";
+			happyThankYou += "Burger? Lol, I hope you get a hard time in the bathroom tonight, fatass. \n";
+		}
+
+		if (ak47.isSelected()) 
+		{ 
+			products += "AK-47 \n"; 
+			happyThankYou += "AK-47? Give up, the 40 virgins are warcraft players. \n";
+		}
+
+		if (m16.isSelected()) 
+		{ 
+			products += "M16 \n";
+			happyThankYou +=  "Rambo...? Hahaha, at least you've got the middle age crisis in common. \n";
+		}
+
+		if (hipsterShirt.isSelected()) 
+		{ 
+			products += "Hipster_-_Shirt \n";
+			happyThankYou += "This shirt was made in China for $0.10 and you buy it for $100? Thanks for showing your critical spirit, sucker! \n"; 
+		}
+
+		if (products.equals(""))
+		{
+			happyThankYou += "Nothing? I hope you burn in hell, miserable basterd! \n";
+		}
+
+		products += "Here's watchu bought: \n" + products + happyThankYou;
+
+		JOptionPane.showMessageDialog(buttonOK, products, "---ORDER SUMMARY---", JOptionPane.INFORMATION_MESSAGE);
+
+		burger.setSelected(false);
+		ak47.setSelected(false);
+		m16.setSelected(false);
+		hipsterShirt.setSelected(false);
+
+		small.setSelected(true);
 	}
 }
