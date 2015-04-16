24476167ef31c80b6e925c73ecc6c38568084b48
otamm <oivatom@gmail.com>
2015-04-15 21:22:12 -0300

    Initial commit

---
+++ delivery.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+
+public class Delivery extends JFrame
+{
+	public static void main(String[] args*)
+	{
+		new Delivery();
+	}
+}
